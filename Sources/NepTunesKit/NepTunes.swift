c0fe5b9128293df6512e0672a1ae16c5e172bf36
Adam Różyński <adam@micropixels.pl>
2021-09-06 23:18:45 +0200

    🔥 changed the api

--- Sources/NepTunesKit/NepTunes.swift
+++ Sources/NepTunesKit/NepTunes.swift
@@ -6,13 +6,26 @@
 
 import Foundation
 
+public struct NepTunesPlayerState {
+    public let playbackState: PlayerPlaybackState
+    public let playerType: PlayerType
+    public let currentTrack: Track?
+    public let shuffle: Bool
+    public let volume: Int
+    public let repeatMode: RepeatMode
+
+    public init(playbackState: PlayerPlaybackState, playerType: PlayerType, currentTrack: Track? = nil, shuffle: Bool, volume: Int, repeatMode: RepeatMode) {
+        self.playbackState = playbackState
+        self.playerType = playerType
+        self.currentTrack = currentTrack
+        self.shuffle = shuffle
+        self.volume = volume
+        self.repeatMode = repeatMode
+    }
+}
+
 public protocol NepTunes {
-    var volume: Int { get }
-    var playbackState: PlayerPlaybackState { get }
-    var playerType: PlayerType { get }
-    var currentTrack: Track? { get }
-    var shuffle: Bool { get }
-    var repeatMode: RepeatMode { get }
+    var playerState: NepTunesPlayerState? { get }
     func nextTrack()
     func backTrack()
     func playPause()
